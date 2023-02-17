1aee2b7e8bc155b7394371a39460a0a8439b589b
RahulSabui <sabuirahul90@gmail.com>
2023-02-17 18:00:39 +0530

    updat echnge

--- App/Middleware/passport.js
+++ App/Middleware/passport.js
@@ -4,18 +4,11 @@ const client = new OAuth2Client("977959450221-3dsvofuc5otbh632sua2ef6940lq8u37.a
 
 exports.verifyUser = async (req, res, next) => {
     try {
-        const token = req?.headers?.token
-        if (token) {
-            const ticket = await client.verifyIdToken({
-                idToken: token,
-                audience: "977959450221-3dsvofuc5otbh632sua2ef6940lq8u37.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
-                // Or, if multiple clients access the backend:
-                //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-            });
-            const payload = ticket.getPayload();
-            const UserData = await User.findOne({ where: { email: payload.email } })
+        const userJson = req?.body?.user
+        if (userJson) {
+
+            const UserData = await User.findOne({ where: { email: userJson.email } })
             req.User = UserData;
-            console.log(UserData.id);
             next()
         } else {
             return res.status(400).json({
