29434963c11381c160dfda3920079a8fb922d5a7
RahulSabui <sabuirahul90@gmail.com>
2023-02-18 21:21:10 +0530

    update chnges

--- Router/details.js
+++ Router/details.js
@@ -5,8 +5,8 @@ const DetailsController = require("../App/Controller/DetailController")
 const UserController = require("../App/Controller/UserController")
 const passport = require("../App/Middleware/passport")
 /* GET home page. */
-router.post('/create',  DetailsController.createDetails);
-router.get('/details', DetailsController.getDetail);
-router.get('/match/details',  DetailsController.MatchersDetails);
+router.post('/create',  passport.verifyUser, DetailsController.createDetails);
+router.post('/details', passport.verifyUser, DetailsController.getDetail);
+router.post('/match/details', passport.verifyUser, DetailsController.MatchersDetails);
 
 module.exports = router;
